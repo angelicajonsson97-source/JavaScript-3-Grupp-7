62b0fbce8583a5519b7b09618e151eb32a8254ee
William Engman <williamengman24@gmail.com>
2026-05-18 13:06:34 +0200

    forgot to save file that was commited

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -5,16 +5,11 @@ import useFetch from '../utils/useFetch'
 
 
 RecipePage.route = {
-<<<<<<< HEAD
   path: '/recipe/:slug' //add slug
-=======
-  path: '/recipe'
->>>>>>> 5adb672cdaf0d3f86791939bce33f46cbce208b2
 }
 
 export default function RecipePage() {
 
-<<<<<<< HEAD
   const { slug } = useParams();
 
   //const slug = "classic-escargot-from-france";
@@ -22,11 +17,4 @@ export default function RecipePage() {
   const { recipe, loading, error, update } = useFetch('/api/recipes/' + slug);
 
   
-=======
-  //const { slug } = useParams();
-
-  const slug = "classic-escargot-from-france";
-
-  const [recipe, setRecipe] = useFetch(slug);
->>>>>>> 5adb672cdaf0d3f86791939bce33f46cbce208b2
 }
