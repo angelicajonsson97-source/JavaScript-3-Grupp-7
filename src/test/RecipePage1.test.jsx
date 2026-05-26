26445ddf6ab83bace50e6cc1eb3d3f1ace97c4c9
William Engman <williamengman24@gmail.com>
2026-05-26 23:56:32 +0200

    updated test file so it skips the new  use hooks and only tests for the rendered titles. also removed file i used as placeholder/temp save when implementing the new fetch hooks.

--- src/test/RecipePage1.test.jsx
+++ src/test/RecipePage1.test.jsx
@@ -5,24 +5,49 @@ import RecipePage1 from '../pages/RecipePage1';
 
 describe('RecipePage', () => { 
 
-  //nullify useFetch
-  vi.mock('../utils/useFetch', () => ({
+    //nullify fetches
+  vi.mock('../utils/useRecipe', () => ({
     default: () => ({ 
-      data: [
-        {
-          data: [
-            {
-              recipe_ingredients: [],
-              recipe_steps: [],
-              commments: [],
-            }
-          ]
-        }
-      ],
+      data: [{
+        data: [{
+          id: 1,
+          title: "Test recipe",
+          recipe_ingredients: [],
+          recipe_steps: [],
+          }]
+        }],
       loading: false,
       error: null,
     })
   }))
+  vi.mock('../utils/useComments', () => ({
+    default: () => ({ 
+      comments: [],
+      loadingCO: false,
+      errorCO: null,
+      refetchComments: vi.fn(),
+      setComments: vi.fn(),
+    })
+  }))
+  vi.mock('../utils/useRatings', () => ({
+    default: () => ({ 
+      ratings: [],
+      loadingRA: false,
+      errorRA: null,
+      refetchRatings: vi.fn(),
+      setRatings: vi.fn(),
+    })
+  }))
+  vi.mock('../utils/useAverageRating', () => ({
+    default: () => ({ 
+      comments: [],
+      loadingAV: false,
+      errorAV: null,
+      refetchAverageRating: vi.fn(),
+      setAverageRating: vi.fn(),
+    })
+  }))
+
 
   it('shows ingredients, instructions and comment titles', () => { 
     render(<RecipePage1 />);
